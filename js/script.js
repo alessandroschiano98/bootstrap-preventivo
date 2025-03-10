fcff6a43af7483a07166367dbe48f15f6d7f2a22
alessandroschiano98 <schianoski98@gmail.com>
2025-03-10 12:59:20 +0100

    INIZIO JAVASCRIPT (constatare come produrre il meccanismo di inserimento typeofworks ed eventuale prezzo stampato ecc)

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+
 // FORM INPUT 
 const userForm = document.getElementById("user-form")
 console.log(userForm);
@@ -13,7 +14,25 @@ console.log(emailLabel);
 
 const codeLabel = document.getElementById("discountcode")
 
-userForm.addEventListener("buttonCalcolaPrev", gestisciUserForm); 
+const typeOfWorkSelect = document.getElementById("typeOfWork")
+console.log(typeOfWorkSelect);
+
+const buttonCalcolaSubmit = document.getElementById ("buttonCalcola")
+console.log(buttonCalcolaSubmit);
+
+const finalPriceResult = document.getElementById ("finalPrice")
+console.log(finalPriceResult);
+
+/*
+// PREZZI ORARI 
+const commissioni = {
+    1: 20.50, // Sviluppo backend (22 ore) 20,50€ x ora
+    2: 15.30, // Sviluppo frontend (32 ore) 15,30€ x ora
+    3: 33.60  // Project analysis (24 ore) 33,60€ x ora
+};
+*/
+
+userForm.addEventListener("buttonCalcola", gestisciUserForm);
 
 function gestisciUserForm(event) {
     event.preventDefault();
@@ -21,10 +40,9 @@ function gestisciUserForm(event) {
     const name = nameLabel.value;
     const lastname = lastnameLabel.value;
     const email = emailLabel.value;
+    const discountcode = codeLabel.value;
+    const typeOfWork = typeOfWorkSelect.value;
 
-    const baseOrediLavoro = 0.21;
-    const ticket = parseFloat(0.21 * kmLabel.value)
-    console.log(ticket);
 
 
 };
