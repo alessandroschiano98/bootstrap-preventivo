99ea7722154885f903dcbe4bfb2b97016269a9be
alessandroschiano98 <schianoski98@gmail.com>
2025-03-13 18:59:54 +0100

    FIX (mi ero dimenticato di applicare il bold alle  cifre intere)

--- js/script.js
+++ js/script.js
@@ -69,6 +69,7 @@ function gestioneUserForm(event) {
     // Viene creata una lista di codici sconto validi:
     const discount25Code = ["YHDNU32", "JANJC63", "PWKCN25", "SJDPO96", "POCIE24"];
 
+
     // Poi viene verificato se il codice inserito è presente in questa lista:
     if (discount25Code.includes(codeLabel.value)) {
         finalPrice = priceOfWorks - (finalPrice * 25 / 100);
@@ -79,14 +80,23 @@ function gestioneUserForm(event) {
         alert("Hai inserito un codice sconto errato");
     }
 
-    // Se l'lemento checkPolicy (HTML) risulta false allora esce alert
+    // Se l'elemento checkPolicy (HTML) risulta false allora esce alert
     if (document.getElementById("checkPolicy").checked === false) {
         alert("Accetta la policy per proseguire");
     }
 
+    // Convertiamo il prezzo in stringa con 2 decimali
+    const prezzoStringa = finalPrice.toFixed(2); 
+
+    // Separiamo la parte intera da quella decimale
+    const parteIntera = prezzoStringa.split(".")[0];
+    const parteDecimale = prezzoStringa.split(".")[1];
 
     // Stampare il risultato:
     finalPriceResult.innerText = `€ ${finalPrice.toFixed(2)}`;
+
+    // Inseriamo il risultato con il bold solo sulla parte intera
+    finalPriceResult.innerHTML = `<strong>€${parteIntera}</strong>.${parteDecimale}`;
 
     // il metodo .toFixed(2) viene utilizzato per formattare un numero con un numero fisso di cifre decimali. Il valore tra parentesi (in questo caso 2) specifica il numero di decimali desiderati.
 }
